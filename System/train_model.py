9963184b7bfe38b8f041809092fb32084411f5f5
kabilraya <roykabil99@gmail.com>
2025-08-28 15:30:51 +0545

    naive bayes class from scratch and training of model

--- System/train_model.py
+++ System/train_model.py
@@ -30,12 +30,12 @@ def tokenize(text:str) -> List[str]:
     tokens = TOKEN_RE.findall(text)
     return [t for t in tokens if t not in STOPWORDS and len(t)>1]
 
-def train_test_split_idx(n:int,test_size:float=0.2,seed:int=42):
-    rng = np.random.default_rng(seed)
-    idx = np.arrange(n)
-    rng.shuffle(idx)
-    test_n = int(round(n*test_size))
-    return idx[test_n:],idx[:test_n]
+# def train_test_split_idx(n:int,test_size:float=0.2,seed:int=42):
+#     rng = np.random.default_rng(seed)
+#     idx = np.arrange(n)
+#     rng.shuffle(idx)
+#     test_n = int(round(n*test_size))
+#     return idx[test_n:],idx[:test_n]
 
 class NaiveBayesImplementation:
     def __init__(self,alpha:float=1.0):
@@ -58,4 +58,48 @@ class NaiveBayesImplementation:
         total_docs = len(y)
         counts = Counter(y)
         self.class_priors_ = {c:math.log(counts[c]/total_docs) for c in classes }
-        
+        #so this function basically creates a dictionary that stores classes:no of occurences
+        #also another dictionary that stores unique words with unique identifiers from the caption
+
+        V = len(vocab)
+        word_counts = {c: np.zeros(V,dtype=int) for c in classes}
+        total_tokens = {c: 0 for c in classes}
+
+        for tokens, cls in zip(X_tokens,y):
+            for t in tokens:
+                j = vocab.get(t)
+                if j is not None:
+                    word_counts[cls][j] +=1
+                    total_tokens[cls] +=1
+        self.likelihoods_ = {}
+        for c in classes:
+            denom = total_tokens[c] + self.alpha * V
+            self.likelihoods_[c] = {j:math.log((cnt + self.alpha)/denom) for j,cnt in enumerate(word_counts[c])}
+
+    def predict_one(self,tokens:List[str])->str:
+        idxs = [self.vocab_[t] for t in tokens if t in self.vocab_]
+        best_c, best_score = None, -1e18
+
+        for c in self.classes_:
+            score = self.class_priors_[c]
+            like_c = self.likelihoods_[c]
+            for j in idxs:
+                score+=like_c[j]
+            if score>best_score:
+                best_c, best_score = c, score
+        return best_c
+
+    def predict(self,X_tokens:List[List[str]]):
+        return [self.predict_one(t) for t in X_tokens]
+
+    def to_json(self) -> str:
+        model = {
+            "alpha" :self.alpha,
+            "classes" : self.classes_,
+            "vocab" : self.vocab_,
+            "class_priors_log" : self.class_priors_,
+            "likelihoods_log" : {c: [self.likelihoods_[c][j] for j in range(len(self.vocab_))] for c in self.classes_},
+        }   
+        return json.dumps(model)
+
+
